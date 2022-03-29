3bc1dbbb5723e57193d0b5f004f82aa379610331
Nader Debbabi <nader.debbabi.30@gmail.com>
2022-03-29 17:50:51 -0400

    Created the app layout

--- webapp/src/app/app-routing.module.ts
+++ webapp/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppLayoutComponent } from './core/components/app-layout/app-layout.component';
 
-const routes: Routes = [];
+
+const APP_ROUTES: Routes = [
+  {
+    path: 'app',
+    component: AppLayoutComponent
+  }
+];
+
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [ RouterModule.forRoot(APP_ROUTES) ],
+  exports: [ RouterModule ]
 })
 export class AppRoutingModule { }
